d4687d970eb54c8e91b214c95cf0b698c7f112c5
KNobles <nobles-k@hotmail.com>
2022-12-15 14:57:56 +0100

    added upload and download

---
+++ dags/kpmg-pipeline/scraper/ocr_fr_detect.py
@@ -0,0 +1,46 @@
+from langdetect import detect
+import camelot
+import os
+
+def ocr_fr_detect_v1(file):
+    """ 
+    This function takes a pdf file as an input and outputs a txt file with the same name.
+    The txt file contains only the french text contained in the pdf document.
+    Takes approximatly 20 seconds for 6 pages
+    """
+    vowels = ['a','e','i','o','u']
+    fr = []
+    # check the file extension
+    if file.endswith(".pdf"):
+        #print(file) # debug, prints filename
+        tables = camelot.read_pdf(file, flavor='stream' , pages= 'all', edge_tol=0)
+        # for every detected table (page and text structure)
+        for i in range(len(tables)):
+            # make a df
+            data = tables[i].df
+            # replace new line (\n) with space
+            data.replace('\\n',' ',regex=True, inplace = True)
+            # for every column detected
+            for j in range(len(data.columns)):
+                # put all the text of that column in a list # this takes also out empty rows and lone numbers (as pagenumber)
+                text_list = [x for x in tables[i].df[j].values if x != '' if not x.isdigit()] 
+                # convert the list to text
+                text = (' '.join(text_list))
+                # if there is at least one vowel (we cannot detect language for numbers)
+                if any(char in vowels for char in text):
+                    # detect language
+                    language = detect(text)
+                    if language == 'fr':
+                        fr.append(text)
+                else:
+                    # print(i,j, ': ',text ) # used for debug
+                    if len(text)>1:
+                        # add rows that contains no vowels, longer than one digit to the french version
+                        fr.append(text)
+        # Outputs the french text in a text file
+        text_file = os.path.splitext(file)[0]
+        with open(f"{text_file}_fr.txt", "w") as output:
+            for row in fr:
+                output.write(row)
+            # little feedback
+            print(f'french extracted into: {file}_fr.txt')
